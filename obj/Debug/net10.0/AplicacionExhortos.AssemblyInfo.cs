181169a963357c5ef8f1350833056c5f2a78251f
Raul Perez Rangel servicio social <rperezr_ss@tribunalesagrarios.gob.mx>
2026-03-23 13:20:47 -0600

    sin boton guardar

--- obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
+++ obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a251059d1e5c400628e094f1472c1edcc2c6180")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91e08ef5031345fb08617819874b890128a4046a")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
