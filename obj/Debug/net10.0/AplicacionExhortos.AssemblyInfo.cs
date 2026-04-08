ba5c668382314fa7afe5de6303da972711ffe2c0
Raul Perez Rangel servicio social <rperezr_ss@tribunalesagrarios.gob.mx>
2026-04-08 10:28:07 -0600

    exhortos recibidos

--- obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
+++ obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13968728bc78c2415997ed5d934e8eb136626eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9918f960d2972e4e03f0072453b24ffe68edf229")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
