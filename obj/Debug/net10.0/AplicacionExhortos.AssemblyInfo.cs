f890546a4442ff2f11c5186e053a0c7a27bd75ff
Raul Perez Rangel servicio social <rperezr_ss@tribunalesagrarios.gob.mx>
2026-04-10 13:12:32 -0600

    exhortos pendientes

--- obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
+++ obj/Debug/net10.0/AplicacionExhortos.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+590c693a91d8910f41304ba9bb914e4a600fa3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34722b5fb899a96a051d72dc6c7f8ad941fa78f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AplicacionExhortos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
